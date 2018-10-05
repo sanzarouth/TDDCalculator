f3da6aba8144b023b05df5154a244e51b8f9d71a
sanzarouth <sanzarouth@hotmail.com>
2018-10-05 15:02:36 +0100

    Added rotating screen test for advanced calculator

--- app/src/androidTest/java/com/example/sanzarouth/tddtest/InstrumentedTestAdvancedActivity.java
+++ app/src/androidTest/java/com/example/sanzarouth/tddtest/InstrumentedTestAdvancedActivity.java
@@ -1,9 +1,15 @@
 package com.example.sanzarouth.tddtest;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import com.example.sanzarouth.tddtest.View.AdvancedActivity;
 
@@ -41,6 +47,29 @@ public class InstrumentedTestAdvancedActivity {
         Espresso.onView(withId(R.id.advancedInput)).perform(ViewActions.typeText("(3+3)*3-p)"));
         Espresso.onView(withId(R.id.calculateBtn)).perform(ViewActions.click());
         Espresso.onView(withId(R.id.advancedResultView)).check(matches(withText("NaN")));
+    }
+
+    @Test
+    public void canRotateScreenAndSaveState() {
+        Espresso.onView(withId(R.id.advancedInput)).perform(ViewActions.typeText("(3+3)*(3-1)"));
+        Espresso.onView(withId(R.id.calculateBtn)).perform(ViewActions.click());
+        Espresso.onView(withId(R.id.advancedResultView)).check(matches(withText("12.0")));
+
+        rotateScreen();
+
+        Espresso.onView(withId(R.id.advancedResultView)).check(matches(withText("12.0")));
+
+    }
+
+    private void rotateScreen() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        int orientation = context.getResources().getConfiguration().orientation;
+
+        Activity activity = activityRule.getActivity();
+        activity.setRequestedOrientation(
+                (orientation == Configuration.ORIENTATION_PORTRAIT) ?
+                        ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE :
+                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
 
